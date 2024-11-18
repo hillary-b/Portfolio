4772d37517bc10ca597b2608ea75934ba42fe89c
hillary-b <31204891+hillary-b@users.noreply.github.com>
2024-11-18 12:34:08 +0000

    Create Car.java

---
+++ Car Park System/Car.java
@@ -0,0 +1,20 @@
+package com.fdmgroup.carPark;
+
+public class Car {
+	private String regNum;
+	
+	
+
+	public Car(String regNum) {
+		super();
+		this.regNum = regNum;
+	}
+
+	public String getRegNum() {
+		return regNum;
+	}
+
+	public void setRegNum(String regNum) {
+		this.regNum = regNum;
+	}
+}
